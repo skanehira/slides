e3eb073471611330aeb41330e831f797d7d5a963
skanehira <sho19921005@gmail.com>
2022-08-17 08:45:55 +0900

    deploy: vim-k8s-plugin

--- scripts/update_index_html.sh
+++ scripts/update_index_html.sh
@@ -4,8 +4,13 @@ cd contents || exit 1
 dir=$(find * -maxdepth 0 -type f)
 cd "$OLDPWD" || exit 1
 
+echo '# My Slides' > public/index.md
+
 for d in $dir; do
-  echo "- https://skanehira.github.io/slides/${d%.md}" >> public/index.md
+  title=$(grep "^#" "contents/${d}" | head -n 1)
+  # remove header(#)
+  title=${title//\# /}
+  echo "- [${title}](https://skanehira.github.io/slides/${d%.md})" >> public/index.md
 done
 
 npx marked -o public/index.html -i public/index.md
